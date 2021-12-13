692fe9341a45ad37e749d8161b3745a0ad149219
Ea Francisco <emberkasai04@gmail.com>
2021-12-12 20:24:14 -0500

    add initial songs to array

---
+++ js/player.js
@@ -0,0 +1,51 @@
+const songList = [
+    {
+    title:`Dumb Blonde`,
+    artist:`Avril Lavigne`,
+    duration:`03:35`,
+    album:`Head Above Water`,
+    genre:`pop`,
+    songSrc: `/music/blonde.mp3`,
+},
+{
+    title:`Bomb`,
+    artist:`AleXa`,
+    duration:`03:23`,
+    album:`Bomb (Single)`,
+    genre:`kpop`,
+    songSrc: `/music/bomb.mp3`,
+},
+{
+    title:`Butter`,
+    artist:`BTS`,
+    duration:`02:45`,
+    album:`Butter (Single)`,
+    genre:`kpop`,
+    songSrc: `/music/butter.mp3`,
+},
+{
+    title:`Dog Days Are Over`,
+    artist:`Florence & the Machines`,
+    duration:`03:45`,
+    album:`Lungs`,
+    genre:`alternative`,
+    songSrc: `/music/dog.mp3`,
+},
+{
+    title:`Fire`,
+    artist:`2NE1`,
+    duration:`03:44`,
+    album:`Debut (Single)`,
+    genre:`kpop`,
+    songSrc: `/music/fire.mp3`,
+},
+{
+    title:`Go!!!`,
+    artist:`FLOW`,
+    duration:`04:05`,
+    album:`Haikyuu Soundtrack`,
+    genre:`anime`,
+    songSrc: `/music/go.mp3`,
+}
+]
+
